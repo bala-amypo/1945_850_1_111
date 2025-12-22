08b62f72b4621346db927ec1b385c9b5f2c628f5
bala-amypo <bala@amypo.in>
2025-12-22 05:51:08 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,27 +1,5 @@
- package com.example.demo.config;
+package com.example.demo.security;
 
-import io.swagger.v3.oas.models.OpenAPI;
- import io.swagger.v3.oas.models.info.Info;
- import io.swagger.v3.oas.models.security.SecurityRequirement;
- import io.swagger.v3.oas.models.security.SecurityScheme;
- import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+public class SwaggerConfig {
 
-@Configuration
- public class SwaggerConfig {
-
-     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-               .info(new Info()
-                       .title("Hostel Roommate Compatibility Matcher")
-                      .version("1.0"))
-              .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-             .components(new io.swagger.v3.oas.models.Components()
-                    .addSecuritySchemes("bearerAuth",
-                            new SecurityScheme()
-                                       .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                         .bearerFormat("JWT")));
-     }
- }
+}
