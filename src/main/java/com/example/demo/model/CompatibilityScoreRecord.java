dd46534f8af87f30b5c8136765ab70146ed2fc02
bala-amypo <bala@amypo.in>
2025-12-26 15:06:20 +0000

    User commit

--- src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
+++ src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
@@ -30,7 +30,7 @@ public class CompatibilityScoreRecord {
 
     private LocalDateTime computedAt = LocalDateTime.now();
 
-    // Getters and Setters
+   
     public Long getId() { return id;
     public void setId(Long id) { this.id = id; }
     public Long getStudentAId() { return studentAId; }
