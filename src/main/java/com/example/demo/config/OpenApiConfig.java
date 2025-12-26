6d43c38f1aa9a80b11b870ddadf99f3156fd31c1
bala-amypo <bala@amypo.in>
2025-12-26 06:39:36 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,16 +4,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Primary
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Hostel Roommate Matcher API")
-                        .version("1.0.0")
+                        .version("1.0.0.0")
                         .description("API documentation for Hostel Roommate Matcher project"));
     }
 }
