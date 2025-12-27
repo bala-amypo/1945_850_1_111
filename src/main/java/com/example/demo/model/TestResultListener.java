5b2e7eb8a1f040920e7fee174aa290fcc8223350
bala-amypo <bala@amypo.in>
2025-12-27 14:11:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/TestResultListener.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestResultListener implements ITestListener {
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Test Suite Started: " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Test Suite Finished: " + context.getName());
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started: " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Passed: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed: " + result.getName());
+        result.getThrowable().printStackTrace();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped: " + result.getName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {}
+}
